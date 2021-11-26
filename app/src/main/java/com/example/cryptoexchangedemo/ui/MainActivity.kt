d93581b3a685b9e436b6e6dfdc68b67f60d8d675
batuhanduvarci <batuhanduvarci@gmail.com>
2021-11-26 21:38:58 +0300

    Update project structure

--- app/src/main/java/com/example/cryptoexchangedemo/ui/MainActivity.kt
+++ app/src/main/java/com/example/cryptoexchangedemo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cryptoexchangedemo
+package com.example.cryptoexchangedemo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cryptoexchangedemo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
