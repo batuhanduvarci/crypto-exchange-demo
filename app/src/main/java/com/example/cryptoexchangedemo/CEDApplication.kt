dff84645c7969ea001ab3197fc454aa93479686d
batuhanduvarci <batuhanduvarci@gmail.com>
2021-11-26 21:39:25 +0300

    Add hilt android application

---
+++ app/src/main/java/com/example/cryptoexchangedemo/CEDApplication.kt
@@ -0,0 +1,11 @@
+package com.example.cryptoexchangedemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Batuhan Duvarci on 26.11.2021.
+ */
+@HiltAndroidApp
+class CEDApplication : Application() {
+}
