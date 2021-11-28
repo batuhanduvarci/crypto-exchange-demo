6caaa93fc7b988563160e0f6580c27d8f9f57233
batuhanduvarci <batuhanduvarci@gmail.com>
2021-11-28 14:23:59 +0300

    Add coin list service integration

--- app/src/main/java/com/example/cryptoexchangedemo/ui/coinlist/CoinListFragment.kt
+++ app/src/main/java/com/example/cryptoexchangedemo/ui/coinlist/CoinListFragment.kt
@@ -1,11 +1,14 @@
 package com.example.cryptoexchangedemo.ui.coinlist
 
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
 import com.example.cryptoexchangedemo.R
 import com.example.cryptoexchangedemo.databinding.FragmentCoinListBinding
 import com.example.cryptoexchangedemo.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
 
 /**
  * Created by Batuhan Duvarci on 26.11.2021.
@@ -13,17 +16,35 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class CoinListFragment : BaseFragment<FragmentCoinListBinding, CoinListViewModel>(R.layout.fragment_coin_list) {
 
-    override val viewModel: CoinListViewModel by viewModels()
+    override val viewModel: CoinListViewModel by activityViewModels()
+
+    private lateinit var adapter: CoinListAdapter
 
     override fun bind(view: View) = FragmentCoinListBinding.bind(view)
 
     override fun initUserInterface() {
-        //TODO("Not yet implemented")
+        with(binding!!){
+            adapter = CoinListAdapter()
+            coinListRecyclerView.adapter = adapter
+        }
     }
 
     override fun initObservers() {
         viewModel.coinList.observe(viewLifecycleOwner, {
-
+            adapter.submitList(it)
         })
+    }
+
+    override fun startCoroutine(){
+        with(viewLifecycleOwner){
+            lifecycleScope.launch(Dispatchers.IO) {
+                whenStarted {
+                    while (isActive){
+                        viewModel.getCoinList()
+                        delay(2000)
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/cryptoexchangedemo/ui/coinlist/CoinListViewModel.kt
+++ app/src/main/java/com/example/cryptoexchangedemo/ui/coinlist/CoinListViewModel.kt
@@ -2,28 +2,21 @@ package com.example.cryptoexchangedemo.ui.coinlist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cryptoexchangedemo.domain.models.CoinModel
 import com.example.cryptoexchangedemo.repository.coinlist.CoinListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
  * Created by Batuhan Duvarci on 26.11.2021.
  */
 @HiltViewModel
-class CoinListViewModel @Inject constructor(private val repository: CoinListRepository): ViewModel() {
+class CoinListViewModel @Inject constructor(private val repository: CoinListRepository) :
+    ViewModel() {
 
     private val _coinList = MutableLiveData<List<CoinModel>>()
     val coinList: MutableLiveData<List<CoinModel>> get() = _coinList
 
-    init {
-        getCoinList()
-    }
+    suspend fun getCoinList() = _coinList.postValue(repository.getCoinList())
 
-    fun getCoinList() = viewModelScope.launch(Dispatchers.IO) {
-        _coinList.postValue(repository.getCoinList())
-    }
 }
