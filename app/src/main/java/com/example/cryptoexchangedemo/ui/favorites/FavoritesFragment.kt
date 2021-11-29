a154e724f860380e2e60c7f4af6880e5cd1864a0
batuhanduvarci <batuhanduvarci@gmail.com>
2021-11-29 21:15:03 +0300

    Refactor favorites fragment

--- app/src/main/java/com/example/cryptoexchangedemo/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/cryptoexchangedemo/ui/favorites/FavoritesFragment.kt
@@ -3,17 +3,18 @@ package com.example.cryptoexchangedemo.ui.favorites
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.example.cryptoexchangedemo.R
-import com.example.cryptoexchangedemo.databinding.FragmentFavoritesBinding
+import com.example.cryptoexchangedemo.databinding.FragmentCoinListBinding
 import com.example.cryptoexchangedemo.ui.base.BaseFragment
 
 /**
  * Created by Batuhan Duvarci on 26.11.2021.
  */
-class FavoritesFragment : BaseFragment<FragmentFavoritesBinding, FavoritesViewModel>(R.layout.fragment_favorites) {
+class FavoritesFragment :
+    BaseFragment<FragmentCoinListBinding, FavoritesViewModel>(R.layout.fragment_coin_list) {
 
     override val viewModel: FavoritesViewModel by viewModels()
 
-    override fun bind(view: View) = FragmentFavoritesBinding.bind(view)
+    override fun bind(view: View) = FragmentCoinListBinding.bind(view)
 
     override fun initUserInterface() {
         //TODO("Not yet implemented")
