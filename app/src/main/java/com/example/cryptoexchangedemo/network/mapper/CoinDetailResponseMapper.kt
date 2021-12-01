1dd8620957492d22f4a3b00a349bc22db52989f4
batuhanduvarci <batuhanduvarci@gmail.com>
2021-12-01 10:58:11 +0300

    Add coin detail response mapper

---
+++ app/src/main/java/com/example/cryptoexchangedemo/network/mapper/CoinDetailResponseMapper.kt
@@ -0,0 +1,29 @@
+package com.example.cryptoexchangedemo.network.mapper
+
+import com.example.cryptoexchangedemo.domain.models.CoinDetailModel
+import com.example.cryptoexchangedemo.domain.models.CoinModel
+import com.example.cryptoexchangedemo.domain.util.DomainMapper
+import com.example.cryptoexchangedemo.network.models.CoinDetailResponseModel
+
+/**
+ * Created by Batuhan Duvarci on 30.11.2021.
+ */
+class CoinDetailResponseMapper: DomainMapper<CoinDetailResponseModel, CoinDetailModel> {
+
+    override fun mapToDomainModel(response: CoinDetailResponseModel): CoinDetailModel {
+        response.d[0].fields?.let {
+            return CoinDetailModel(
+                it.buy,
+                it.ddi,
+                it.hig,
+                it.las,
+                it.low,
+                it.pdd,
+                it.sel
+            )
+        }
+        return CoinDetailModel()
+    }
+
+    override fun fromEntityList(initial: List<CoinDetailResponseModel>) = listOf<CoinDetailModel>()
+}
